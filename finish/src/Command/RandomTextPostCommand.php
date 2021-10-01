4d8d83ec2ebd04b0f727af64fd7129e00604077d
drLao <korum@list.ru>
2021-10-01 12:09:27 -0400

    add command for random text post generation

--- finish/src/Command/RandomTextPostCommand.php
+++ finish/src/Command/RandomTextPostCommand.php
@@ -2,6 +2,7 @@
 
 namespace App\Command;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -12,30 +13,55 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 class RandomTextPostCommand extends Command
 {
     protected static $defaultName = 'app:random-text-post';
-    protected static $defaultDescription = 'Add a short description for your command';
+    protected static $defaultDescription = 'Random text post generator';
+
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+
+        parent::__construct();
+    }
 
     protected function configure(): void
     {
         $this
-            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
-            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
+            ->addArgument('your-name', InputArgument::OPTIONAL, 'Your name')
+            ->addOption('caps', null, InputOption::VALUE_NONE, 'All in CAPS')
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
-        $arg1 = $input->getArgument('arg1');
+        $yourName = $input->getArgument('your-name');
 
-        if ($arg1) {
-            $io->note(sprintf('You passed an argument: %s', $arg1));
+        if ($yourName) {
+            $io->note(sprintf('You passed a name: %s', $yourName));
         }
 
-        if ($input->getOption('option1')) {
-            // ...
+        $postBits = [
+            ' posuit ', ' week ', ' insect ', ' ferrum ', ' aer ', ' dentium ', ' omnis ', ' sublust ', ' vitro ',
+            ' mortuus ', ' caracterem ', ' dirige ', ' tunica ', ' perveniunt ', ' nasum ', ' risu ', ' pascuntur ',
+            ' statera ', ' exercitium ', ' call ', ' adepto ', ' gavisus ', ' proeorous ', ' continent ',
+            ' repentino ', ' vimus ', ' current ', ' postquam ', ' hauriret ', ' fuge ', ' latere ', ' tangerent ',
+            ' fieri ', ' fera ', ' vitae ', ' sensi ', ' frigus ', ' figistrus ', ' castra ', ' aquam ', ' obtinuit ',
+            ' festinate ', ' violet ', ' celebre ',
+        ];
+
+        $postRandomText = "";
+
+        for ($i = 0; $i < 55; $i++) {
+            $postRandomText .= $postBits[array_rand($postBits)];
         }
 
-        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+        if ($input->getOption('caps')) {
+            $postRandomText = strtoupper($postRandomText);
+        }
+
+        $this->logger->info('Random post was generated '.$postRandomText);
+        $io->success($postRandomText);
 
         return Command::SUCCESS;
     }
