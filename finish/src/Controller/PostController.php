898a17f88ccbcd8750fdcca9be5efb2a8475de79
drLao <korum@list.ru>
2021-10-02 12:55:14 -0400

    delete unused imports from PostCont

--- finish/src/Controller/PostController.php
+++ finish/src/Controller/PostController.php
@@ -10,13 +10,9 @@ use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
 use Sentry\State\HubInterface;
 
-use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-use Symfony\Component\Console\Input\ArrayInput;
-use Symfony\Component\Console\Output\BufferedOutput;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 use App\Service\MarkdownHelper;
