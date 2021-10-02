eede8b23fa16b599e413eef57978678983e83f6d
drLao <korum@list.ru>
2021-10-02 06:38:45 -0400

    add Post Entity

---
+++ finish/src/Entity/Post.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PostRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PostRepository::class)
+ */
+class Post
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $slug;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $post_body;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $postedAt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getPostBody(): ?string
+    {
+        return $this->post_body;
+    }
+
+    public function setPostBody(string $post_body): self
+    {
+        $this->post_body = $post_body;
+
+        return $this;
+    }
+
+    public function getPostedAt(): ?\DateTimeInterface
+    {
+        return $this->postedAt;
+    }
+
+    public function setPostedAt(?\DateTimeInterface $postedAt): self
+    {
+        $this->postedAt = $postedAt;
+
+        return $this;
+    }
+}
