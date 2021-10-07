f9b147548367452c60c63646e9644d2d33aeaf58
drLao <korum@list.ru>
2021-10-07 03:30:51 -0400

    make some improvments in CommentFixtures

--- finish/src/DataFixtures/CommentFixtures.php
+++ finish/src/DataFixtures/CommentFixtures.php
@@ -2,52 +2,28 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Comment;
-use App\Entity\Post;
 use App\Factory\CommentFactory;
 use App\Factory\PostFactory;
-use App\Service\RandomTextGeneratorHelper;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 
-class CommentFixtures extends Fixture
+class CommentFixtures extends Fixture implements DependentFixtureInterface
 {
-//    private $postFactory;
-//
-//    public function __construct(PostFactory $postFactory)
-//    {
-//        $this->postFactory = $postFactory;
-//    }
-
-    private $randomTextGeneratorHelper;
-
-    public function __construct(RandomTextGeneratorHelper $randomTextGeneratorHelper)
-    {
-        $this->randomTextGeneratorHelper = $randomTextGeneratorHelper;
-    }
-
 
     public function load(ObjectManager $manager): void
     {
-        $comment = new Comment();
-        $comment->setCommentBody($this->randomTextGeneratorHelper->generateRandomWords(25));
-        $comment->setUsername('dr_Lao');
+        CommentFactory::createMany(100, function() {
+            return [
+                'post' => PostFactory::random()
+            ];
+        });
+    }
 
-
-        $post = new Post();
-        $post->setTitle($this->randomTextGeneratorHelper->generateRandomWords(7));
-        $post->setPostBody($this->randomTextGeneratorHelper->generateRandomWords(45));
-
-
-
-        $manager->persist($post);
-        $comment->setPost($post);
-        $manager->persist($comment);
-        $manager->flush();
-
-
-
-
-        //CommentFactory::createMany(3);
+    public function getDependencies()
+    {
+        return [
+            PostFixtures::class,
+        ];
     }
 }
